f9783d7a5b0b209c1757538f1148dc718451da66
Casey Poon <caseypoon2020@u.northwestern.edu>
2020-01-21 16:59:27 -0600

    Implement static shopping cart diplay

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import React, { useEffect, useState } from 'react';
+import { Image, Button, Navbar } from 'rbx';
 import ProductGrid from './Components/ProductGrid'; 
-
+import ShoppingCart from './Components/ShoppingCart'
+import Sidebar from "react-sidebar"
 
 const App = () => {
   const [data, setData] = useState({});
   const products = Object.values(data);
+
+
+  const [cartOpen, setCartOpen] = useState(false);
+
+
   useEffect(() => {
     const fetchProducts = async () => {
       const response = await fetch('./data/products.json');
@@ -15,7 +22,23 @@ const App = () => {
   }, []);
 
   return (
-    <ProductGrid products={products} />
+    <React.Fragment>
+      <Sidebar
+        styles={{sidebar: { backgroundColor: "white" }}}
+        sidebar={<ShoppingCart />}
+        open={cartOpen}
+        pullRight={true}>
+      <Navbar>
+        <Navbar.Segment align="end">
+        { cartOpen?
+          ( <Button> <Image onClick ={() => setCartOpen(false)} src={'data/icons/cart.png'} /> </Button>):
+          ( <Button> <Image onClick ={() => setCartOpen(true)} src={'data/icons/cart.png'} /> </Button>)
+        }
+        </Navbar.Segment>
+      </Navbar>
+      <ProductGrid products={products} />
+    </Sidebar>
+    </React.Fragment>
   );
 };
 
---
+++ src/Components/ShoppingCart.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import 'rbx/index.css';
+import { Card } from 'rbx';
+
+
+const ShoppingCart = ({ cartOpen }) => {
+    return (
+        <React.Fragment>
+            <Card>Hello World, I am a Shopping Cart!</Card>
+        </React.Fragment>
+    );
+}
+
+export default ShoppingCart
